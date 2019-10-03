dd227b6a715046433e838bbb654002b1b1290aa8
edwinisnow <edwin04@gmail.com>
2019-10-04 00:28:55 +0100

    Handlers

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 
 class TodoInput extends Component {
+
     render() {
+        const { item, handleChange, handleSubmit } = this.props;
         return (
             <div className="card card-body my-3">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text bg-primary text-white">
                                 <i className="fas fa-book"></i>
                             </div>
                         </div>
-                        <input type="text" className="form-control text-capitalize" placeholder="add a todo item" />
+                        <input type="text" className="form-control text-capitalize" placeholder="add a todo item"
+                            onChange={handleChange}
+                            value={item} />
                     </div>
                     <button className="btn btn-block btn-primary mt-3" type="submit">Add Item</button>
                 </form>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,15 @@ import TodoItem from './TodoItem';
 
 class TodoList extends Component {
     render() {
+        const { items } = this.props;
         return (
             <ul className="list-group my-5">
                 <h3 className="text-capitalize text-center">Todo List</h3>
-                <TodoItem />
+                {
+                    items.map(item => {
+                        return (<TodoItem key={item.id} title={item.title} />)
+                    })
+                }
                 <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">
                     Clear List</button>
             </ul>

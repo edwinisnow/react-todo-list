9794f8a827f9540319f52e3dcbf80fe956c12798
edwinisnow <edwin04@gmail.com>
2019-10-03 23:04:54 +0100

    Basic Compnents Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div >
       Hello from app component
+      <TodoInput />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                Todo Input Component
+            </div>
+        )
+    }
+}
+
+export default TodoInput
